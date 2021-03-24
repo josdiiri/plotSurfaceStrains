c72ca3d7084a1e516cb90ad3dc523a59000fc64f
Pepe Iriarte-Diaz <josdiiri2@gmail.com>
2021-03-23 21:33:12 -0500

    Revert "Revert "Surface Strains""
    This reverts commit 82bc43c3b42dfb626e09e08a000afb5cc185b91d.

---
+++ PrincStrains.m
@@ -0,0 +1,59 @@
+function [PSmagnitude,PSdirection,rotS] = PrincStrains (S,Rm)
+
+% This functions employ a rotation matrix (Rm) to
+% (i)   create a strain tensor (e) from vectors entered as XX,YY,ZZ,XY,YZ,XZ
+% (ii)  perform a transformation of the strains from xyz to x'y'z' coord
+%       system to create a tensor called (eprime)
+% (iii) calculate principal strains (D) and (V) from (eprime)
+%
+% Arguments (INPUT)
+%  S  -  1 by 6 raw vector with normal and shear strains entered as
+%        [XX, YY, ZZ, XY, YZ, XZ]
+%  Rm -  3 by 3 array with the rotation matrix (i.e., direction cosines
+%        matrix)
+%
+% Arguments (OUTPUT)
+%  Magnitude       - 3 by 1 array of the magnitude of the Principal
+%                    strains (PS), which corresponds to the eigenvalues of
+%                    the rotated strain tensor
+%  Direction       - 3 by 3 array of the direction of the principal
+%                    strains [e.g., direction of 1st PS = Direction(:,1) ]
+%  RotStrainTensor - 3 by 3 array with the resultant strain tensor after
+%                    rotation
+%
+% Note that this uses the strain transformation method of Ameen: i.e.,
+% the shear strains are divided by 2.
+
+nStrains = size(S,1) ;
+PSmagnitude = nan(nStrains,3) ;
+PSdirection = nan(nStrains,9) ;
+rotS = nan(size(S)) ;
+
+for i = 1:nStrains
+    textwaitbar(i, nStrains,'Calculating strains');
+
+    e = [ S(i,1)       S(i,4)/2    S(i,5)/2 ; 
+          S(i,4)/2     S(i,2)      S(i,6)/2 ; 
+          S(i,5)/2     S(i,6)/2    S(i,3) ] ;
+    
+    % transforms strain tensor to new coordinate system
+    % using the direction cosine matrix Rm
+    if Rm==eye(3)
+        e1 = e ;
+    else
+        e1 = Rm * e * Rm';
+    end
+    rotS(i,:) = [e1(1,1) e1(2,2) e1(3,3) e1(1,2) e1(1,3) e1(2,3)] ;
+    
+      
+    if ~all(isnan(e(:)))
+    % calculate PS from original data
+    [Vo,Do] = eig(e1) ; % columns of Vo are eigenvectors
+    [psMag,indO] = sort(diag(Do),'descend') ;
+    psDir = Vo(:,indO) ;
+    [~,psDir] = isRightHandedCS(psDir) ;
+
+    PSmagnitude(i,:) = psMag ;
+    PSdirection(i,:) = psDir(:) ;
+    end
+end
---
+++ loadStrainData.m
@@ -0,0 +1,70 @@
+function [StrainOutput, modelData] = loadStrainData
+
+%% load the 3D MODEL
+modelData = load('model_Human') ; % loaded as modelData
+
+%% Select the STRAIN data
+
+% Here you should select all the models that you want to compare
+[Files,pathName] = uigetfile('*.rpt','multiselect','on') ;
+
+% prepare the output files
+Labels = {'File';'Nodes';'Strains'} ;
+nFiles = numel(Files) ;
+StrainOutput = cell(3,nFiles+1) ;
+StrainOutput(:,1) = Labels ;
+
+clc
+disp('Loading and processing the RPT (strain) files')
+
+% get the data    
+for i = 1:nFiles
+    fileName = Files{i} ;
+    
+    disp(' ')
+    disp(['--> Processing file ' num2str(i) ' of ' num2str(nFiles)])
+    disp(['File: "' fileName '"'])
+    
+    % read the RPT files and extract the data
+    [NodesXYZ,NodesID,strains,strainNodesID,NodesLabels] = ...
+        readRPTfiles(fullfile(pathName,fileName)) ;
+    % NodesXYZ is a n1*3 matrix of the XYZ position of the nodes of the model
+    % NodesID is a n1*1 vector of the ID number of the nodes
+    % strains is a n2*6 matrix of the direct strains
+    %           (e_11,e_22,e_33,e_12,e_13,e_23)
+    % strainNodesID is a n2*2 matrix of the ID of the nodes and the ID of
+    %           the individual elements attached to each node
+    % NodesLabels is a n1*1 vector with the labels identifying the material
+    %           of the model
+    
+    correctionFactor = 1e6 ; % correction factor from strains to microstrains
+    strainData = table(strainNodesID,strains.*correctionFactor,...
+        'VariableNames',{'nodesID','directStrain'}) ;
+    
+    % initialize the output table
+    nodeData = table(NodesID,NodesLabels,NodesXYZ) ;
+        
+    % reduce the data so that we only work with the strain data of the
+    % nodes that matches the nodes of the surface model
+    [redNodeData,redStrainData] = matchModelStrainNodes(modelData.Mand, ...
+        nodeData, strainData, 1) ;
+
+    % calculate the principal strains
+    [PSmag,PSdir] = PrincStrains(redStrainData.directStrain, eye(3)) ;
+    
+    % Because there are multiple elements attached per node, we need to
+    % calculate either the mean or the median so that we can have just one
+    % point per node
+    statistic = 'median' ;
+    averageStrain = averageStrainPerNode(redNodeData.NodesID,...
+        redStrainData.nodesID(:,1),...
+        [redStrainData.directStrain PSmag PSdir], statistic) ;
+    
+    % write the output table
+    outputTable = table(redNodeData.NodesID,averageStrain(:,1:6),averageStrain(:,7:9),...
+        averageStrain(:,10:18),'VariableNames',{'nodesID','directStrain','PSmag','PSdir'}) ;
+
+    StrainOutput(1,i+1) = {fileName(1:end-4)} ;
+    StrainOutput(2,i+1) = {redNodeData} ;
+    StrainOutput(3,i+1) = {outputTable} ;
+end
